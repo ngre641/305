626919169ba0866bacf27d3e270a5473456fd441
Catherine (Xin-Yue) Wei <175383495+cw1zard@users.noreply.github.com>
2026-05-10 12:00:12 +1200

    ball falling correctly, colour change dip switch, reset pb, edited positioning

--- mini_project/mini_project/bouncy_ball.vhd
+++ mini_project/mini_project/bouncy_ball.vhd
@@ -6,7 +6,7 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 ENTITY bouncy_ball IS
 	PORT
-		( pb1, pb2, clk, vert_sync, button_click	: IN std_logic;
+		( pb1, pb2, clk, vert_sync, button_click, reset	: IN std_logic;
           pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
 		  red, green, blue 			: OUT std_logic);		
 END bouncy_ball;
@@ -23,7 +23,7 @@ BEGIN
 
 size <= CONV_STD_LOGIC_VECTOR(8,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,11);
+ball_x_pos <= CONV_STD_LOGIC_VECTOR(180,11);
 
 ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
 					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
@@ -37,19 +37,25 @@ Green <= (not pb2) and (not ball_on);
 Blue <=  not ball_on;
 
 
-Move_Ball: process (vert_sync)  	
+Move_Ball: process (vert_sync, reset)
 begin
-	-- Move ball once every vertical sync
-	if (rising_edge(vert_sync)) then			
-		-- Bounce off top or bottom of the screen
-		ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
-		if (button_click = '1') then
-			ball_y_motion <= CONV_STD_LOGIC_VECTOR(10,10);
-		end if;
-		
-		-- Compute next ball Y position
-		ball_y_pos <= ball_y_pos + ball_y_motion;
-	end if;
+    if (reset = '0') then
+        -- Set your starting point here
+        ball_y_pos <= CONV_STD_LOGIC_VECTOR(240, 10); -- Start near top
+ 
+    elsif (rising_edge(vert_sync)) then
+        -- JUMP LOGIC
+        if (button_click = '1') then
+            ball_y_pos <= ball_y_pos - CONV_STD_LOGIC_VECTOR(10, 10);
+            
+        -- FALLING LOGIC (with bottom edge stop at 470)
+        elsif (ball_y_pos < CONV_STD_LOGIC_VECTOR(470, 10)) then
+            ball_y_pos <= ball_y_pos + CONV_STD_LOGIC_VECTOR(4, 10);
+		else 
+			ball_y_pos <= CONV_STD_LOGIC_VECTOR(470, 10);
+        end if;
+ 
+    end if;
 end process Move_Ball;
 
 END behavior;
--- mini_project/mini_project/db/altpll0_altpll.v
+++ mini_project/mini_project/db/altpll0_altpll.v
@@ -1,24 +1,25 @@
 //altpll bandwidth_type="AUTO" CBX_DECLARE_ALL_CONNECTED_PORTS="OFF" clk0_divide_by=2 clk0_duty_cycle=50 clk0_multiply_by=1 clk0_phase_shift="0" compensate_clock="CLK0" device_family="Cyclone V" inclk0_input_frequency=20000 intended_device_family="Cyclone III" lpm_hint="CBX_MODULE_PREFIX=altpll0" operation_mode="normal" pll_type="AUTO" port_clk0="PORT_USED" port_clk1="PORT_UNUSED" port_clk2="PORT_UNUSED" port_clk3="PORT_UNUSED" port_clk4="PORT_UNUSED" port_clk5="PORT_UNUSED" port_extclk0="PORT_UNUSED" port_extclk1="PORT_UNUSED" port_extclk2="PORT_UNUSED" port_extclk3="PORT_UNUSED" port_inclk1="PORT_UNUSED" port_phasecounterselect="PORT_UNUSED" port_phasedone="PORT_UNUSED" port_scandata="PORT_UNUSED" port_scandataout="PORT_UNUSED" self_reset_on_loss_lock="OFF" width_clock=5 areset clk inclk locked CARRY_CHAIN="MANUAL" CARRY_CHAIN_LENGTH=48
-//VERSION_BEGIN 18.1 cbx_altclkbuf 2018:09:12:13:04:24:SJ cbx_altiobuf_bidir 2018:09:12:13:04:24:SJ cbx_altiobuf_in 2018:09:12:13:04:24:SJ cbx_altiobuf_out 2018:09:12:13:04:24:SJ cbx_altpll 2018:09:12:13:04:24:SJ cbx_cycloneii 2018:09:12:13:04:24:SJ cbx_lpm_add_sub 2018:09:12:13:04:24:SJ cbx_lpm_compare 2018:09:12:13:04:24:SJ cbx_lpm_counter 2018:09:12:13:04:24:SJ cbx_lpm_decode 2018:09:12:13:04:24:SJ cbx_lpm_mux 2018:09:12:13:04:24:SJ cbx_mgl 2018:09:12:13:10:36:SJ cbx_nadder 2018:09:12:13:04:24:SJ cbx_stratix 2018:09:12:13:04:24:SJ cbx_stratixii 2018:09:12:13:04:24:SJ cbx_stratixiii 2018:09:12:13:04:24:SJ cbx_stratixv 2018:09:12:13:04:24:SJ cbx_util_mgl 2018:09:12:13:04:24:SJ  VERSION_END
+//VERSION_BEGIN 25.1 cbx_altclkbuf 2025:10:22:10:31:27:SC cbx_altiobuf_bidir 2025:10:22:10:31:27:SC cbx_altiobuf_in 2025:10:22:10:31:27:SC cbx_altiobuf_out 2025:10:22:10:31:27:SC cbx_altpll 2025:10:22:10:31:27:SC cbx_cycloneii 2025:10:22:10:31:27:SC cbx_lpm_add_sub 2025:10:22:10:31:27:SC cbx_lpm_compare 2025:10:22:10:31:27:SC cbx_lpm_counter 2025:10:22:10:31:27:SC cbx_lpm_decode 2025:10:22:10:31:27:SC cbx_lpm_mux 2025:10:22:10:31:26:SC cbx_mgl 2025:10:22:10:31:44:SC cbx_nadder 2025:10:22:10:31:27:SC cbx_stratix 2025:10:22:10:31:27:SC cbx_stratixii 2025:10:22:10:31:26:SC cbx_stratixiii 2025:10:22:10:31:27:SC cbx_stratixv 2025:10:22:10:31:26:SC cbx_util_mgl 2025:10:22:10:31:27:SC  VERSION_END
 //CBXI_INSTANCE_NAME="mini_project_altpll0_inst_altpll_altpll_component"
 // synthesis VERILOG_INPUT_VERSION VERILOG_2001
 // altera message_off 10463
 
 
 
-// Copyright (C) 2018  Intel Corporation. All rights reserved.
-//  Your use of Intel Corporation's design tools, logic functions 
-//  and other software and tools, and its AMPP partner logic 
+// Copyright (C) 2025  Altera Corporation. All rights reserved.
+//  Your use of Altera Corporation's design tools, logic functions 
+//  and other software and tools, and any partner logic 
 //  functions, and any output files from any of the foregoing 
 //  (including device programming or simulation files), and any 
 //  associated documentation or information are expressly subject 
-//  to the terms and conditions of the Intel Program License 
-//  Subscription Agreement, the Intel Quartus Prime License Agreement,
-//  the Intel FPGA IP License Agreement, or other applicable license
+//  to the terms and conditions of the Altera Program License 
+//  Subscription Agreement, the Altera Quartus Prime License Agreement,
+//  the Altera IP License Agreement, or other applicable license
 //  agreement, including, without limitation, that your use is for
 //  the sole purpose of programming logic devices manufactured by
-//  Intel and sold by Intel or its authorized distributors.  Please
-//  refer to the applicable agreement for further details.
+//  Altera and sold by Altera or its authorized distributors.  Please
+//  refer to the Altera Software License Subscription Agreements 
+//  on the Quartus Prime software download page.
 
 
 
