0b92f86643b6ec16eb41b8edd5377fcc390f8509
ngre641 <ngre641@aucklanduni.ac.nz>
2026-05-12 12:52:02 +1200

    fixed syntax errors

--- mini_project/mini_project/colour_select.vhd
+++ mini_project/mini_project/colour_select.vhd
@@ -5,7 +5,9 @@ USE IEEE.STD_LOGIC_UNSIGNED.all;
 ENTITY colour_select IS
     PORT
     (
-        l1_red, l1_green, l1_blue, l2_red, l2_green, l2_blue, l3_red, l3_green, l3_blue : IN STD_LOGIC;
+        l1_red, l1_green, l1_blue, 
+        l2_red, l2_green, l2_blue, 
+        l3_red, l3_green, l3_blue : IN STD_LOGIC;
         pixel_row, pixel_column : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
         red, green, blue : OUT STD_LOGIC
     );
@@ -14,23 +16,26 @@ END colour_select;
 ARCHITECTURE behavior OF colour_select IS
 
 BEGIN
-    
-    if (l1_red or l1_green or l1_blue) = '1' then
-        red <= l1_red;
-        green <= l1_green;
-        blue <= l1_blue;
-    elsif (l2_red or l2_green or l2_blue) = '1' then
-            red <= l2_red;
+
+    PROCESS(pixel_row, pixel_column)
+    BEGIN
+        IF (l1_red OR l1_green OR l1_blue) = '1' THEN
+            red   <= l1_red;
+            green <= l1_green;
+            blue  <= l1_blue;
+        ELSIF (l2_red OR l2_green OR l2_blue) = '1' THEN
+            red   <= l2_red;
             green <= l2_green;
-            blue <= l2_blue;
-    elsif (l3_red or l3_green or l3_blue) = '1' then
-            red <= l3_red;
+            blue  <= l2_blue;
+        ELSIF (l3_red OR l3_green OR l3_blue) = '1' THEN
+            red   <= l3_red;
             green <= l3_green;
-            blue <= l3_blue;
-    else
-        red <= '0';
-        green <= '0';
-         blue <= '0';
-    end if;
+            blue  <= l3_blue;
+        ELSE
+            red   <= '0';
+            green <= '0';
+            blue  <= '0';
+        END IF;
+    END PROCESS;
 
 END behavior;
