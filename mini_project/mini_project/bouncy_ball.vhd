69b196ee15d773616fb60ed199df27eaa5fe2cf7
ngre641 <ngre641@aucklanduni.ac.nz>
2026-05-12 13:40:32 +1200

    fixed error

--- mini_project/mini_project/bouncy_ball.vhd
+++ mini_project/mini_project/bouncy_ball.vhd
@@ -33,8 +33,8 @@ ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' &
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
 Red <=  pb1;
-Green <= 0;
-Blue <=  0;
+Green <= '0';
+Blue <=  '0';
 
 
 Move_Ball: process (vert_sync, reset)
